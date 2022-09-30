e0d0266b9d6f24d098668b540a833318f5acd0a3
lorphemus <bnk@excess.web.tr>
2022-09-30 16:07:28 +0300

    Assets

---
+++ assets/footer.php
@@ -0,0 +1,68 @@
+<div class="footer__area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-sm-6 lg-mb-30">
+                <div class="footer__area-widget borders">
+                    <h5>HAIR CENTRUM</h5>
+                    <div class="footer__area-widget-about">
+                        <p>Proin libero velit, blandit a metus ac, interdum ultrices enim.</p>
+                        <div class="footer__area-widget-about-social">
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-xl-4 col-lg-4 col-sm-6">
+                <div class="footer__area-widget borders">
+                    <h5>Hizmetlerimiz</h5>
+                    <div class="footer__area-widget-contact">
+                        <ul>
+                            <li><a href="#">Saç Ekimi</a></li>
+                            <li><a href="#">Estetik</a></li>
+                            <li><a href="#">Diş Estetiği</a></li>
+                            <li><a href="#">Obezite Cerrahisi</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xl-4 col-lg-4 col-sm-6 sm-mb-30">
+                <div class="footer__area-widget borders border-r">
+                    <h5>Bize Ulaşın</h5>
+                    <div class="footer__area-widget-contact">
+                        <ul>
+                            <li><a href="#">Abide-i Hürriyet Cad.NO:148 <br> İstanbul / Türkiye</a></li>
+                            <li><a href="#">info@haircentrum.com</a></li>
+                            <li><a href="#">+90 535 550 65 55</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+<!-- Copyright Area Start -->	
+<div class="copyright__area">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-xl-6 col-lg-6 lg-mb-10">
+                <div class="copyright__area-left lg-t-center">
+                    <p>Copyright © 2022 Hair Centrum | Created by <a href="https://www.excessreklam.com">Excess Reklam</a></p>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6">
+                <div class="copyright__area-right t-right lg-t-center">
+                    <ul>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>	
+<!-- Copyright Area End -->
---
+++ assets/header.php
@@ -0,0 +1,94 @@
+<div class="header__area header__sticky">
+    <div class="container custom__container">
+        <div class="header__area-menubar">
+            <div class="header__area-menubar-left">
+                <div class="header__area-menubar-left-logo">
+                    <a href="index.php"><img src="assets/img/logo.png" alt=""></a>
+                </div>
+            </div>
+            <div class="header__area-menubar-center">
+                <div class="header__area-menubar-center-menu menu-responsive">						
+                    <ul id="mobilemenu">
+                        <li><a href="index.php">Anasayfa</a></li>
+                        <li><a href="about.php">Hakkımızda</a></li>
+                        <li class="menu-item-has-children"><a href="#">Hizmetlerimiz</a>
+                            <ul class="sub-menu">
+                                <li><a href="#">Saç Ekimi</a></li>
+                                <li><a href="#">Estetik</a></li>
+                                <li><a href="#">Diş Estetiği</a></li>
+                                <li><a href="#">Obezite Cerrahisi</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">İletişim</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="header__area-menubar-right">
+                <div class="header__area-menubar-right-box">
+                    <div class="header__area-menubar-right-box-search">
+                        <div class="search">	
+                            <span class="header__area-menubar-right-box-search-icon open"><i class="fal fa-globe"></i></span>
+                        </div>
+                        <div class="header__area-menubar-right-box-search-box">
+                            <div style="color: cornsilk; margin: auto;">Dil Seçenekleri</div>
+                            <span class="header__area-menubar-right-box-search-box-icon"><i class="fal fa-times"></i></span>
+                        </div>
+                    </div>
+                    <div class="header__area-menubar-right-sidebar">
+                        <div class="header__area-menubar-right-sidebar-popup-icon"><span></span><span></span><span></span></div>
+                    </div>
+                    <!-- sidebar Menu Start -->
+                    <div class="header__area-menubar-right-sidebar-popup">
+                        <div class="sidebar-close-btn"><i class="fal fa-times"></i></div>
+                        <div class="header__area-menubar-right-sidebar-popup-logo">
+                            <a href="index.php"> <img src="assets/img/Logo.png" alt=""> </a>
+                        </div>
+                        <p>Morbi et tellus imperdiet, aliquam nulla sed, dapibus erat. Aenean dapibus sem non purus venenatis vulputate. Donec accumsan eleifend blandit. Nullam auctor ligula</p>
+                        <div class="header__area-menubar-right-box-sidebar-popup-image">
+                            <img src="assets/img/bar.jpg" alt="">
+                        </div>
+                        <div class="header__area-menubar-right-box-sidebar-popup-contact">
+                            <h4 class="mb-30">İletişim Bilgileri</h4>
+                            <div class="header__area-menubar-right-box-sidebar-popup-contact-item">
+                                <div class="header__area-menubar-right-box-sidebar-popup-contact-item-icon">
+                                    <i class="fal fa-phone-alt"></i>
+                                </div>
+                                <div class="header__area-menubar-right-box-sidebar-popup-contact-item-content">
+                                    <span>Bizi Arayın</span>
+                                    <h6><a href="#">+90 535 550 65 55</a></h6>
+                                </div>
+                            </div>
+                            <div class="header__area-menubar-right-box-sidebar-popup-contact-item">
+                                <div class="header__area-menubar-right-box-sidebar-popup-contact-item-icon">
+                                    <i class="fal fa-envelope"></i>
+                                </div>
+                                <div class="header__area-menubar-right-box-sidebar-popup-contact-item-content">
+                                    <span>Mail Adresimiz</span>
+                                    <h6><a href="#">info@haircentrum.com</a></h6>
+                                </div>
+                            </div>
+                            <div class="header__area-menubar-right-box-sidebar-popup-contact-item">
+                                <div class="header__area-menubar-right-box-sidebar-popup-contact-item-icon">
+                                    <i class="fal fa-map-marker-alt"></i>
+                                </div>
+                                <div class="header__area-menubar-right-box-sidebar-popup-contact-item-content">
+                                    <span>Adresimiz</span>
+                                    <h6><a href="#">Merkez Mah. Abide-i Hürriyet Cad.NO:148 İstanbul / Türkiye</a></h6>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="header__area-menubar-right-box-sidebar-popup-social">
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            </ul>							
+                        </div>
+                    </div>
+                    <div class="sidebar-overlay"></div>
+                    <!-- sidebar Menu Start -->
+                </div>
+                <div class="responsive-menu"></div>
+            </div>
+        </div>
+    </div>
+</div>
